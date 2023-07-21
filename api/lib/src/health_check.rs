01f619cc2a2d4aa8ee605b6cf4cf16ae652bc587
solomonbaez <solomon.james.baez@gmail.com>
2023-07-21 14:42:04 -0700

    /health unit testing

--- api/lib/src/health_check.rs
+++ api/lib/src/health_check.rs
@@ -3,12 +3,36 @@ use actix_web::{
     HttpResponse,
 };
 
+const API_VERSION: &str = "v0.0.1";
+
 pub fn service(cfg: &mut ServiceConfig) {
     cfg.route("/health", web::get().to(health));
 }
 
 async fn health() -> HttpResponse {
     HttpResponse::Ok()
-        .append_header(("version", "0.0.1"))
+        .append_header(("version", API_VERSION))
         .finish()
+}
+
+#[cfg(test)]
+mod tests {
+    use actix_web::http::StatusCode;
+    use super::*;
+
+    #[actix_rt::test]
+    async fn health_check_returns_200() {
+        let response = health().await;
+
+        assert!(response.status().is_success());
+        assert_eq!(response.status(), StatusCode::OK);
+
+        let data = response
+            .headers()
+            .get("version")
+            .and_then(|h| h.to_str().ok());
+
+        assert_eq!(data, Some(API_VERSION));
+    }
+
 }
