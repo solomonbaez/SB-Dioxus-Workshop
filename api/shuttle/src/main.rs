3ded4127a9153ed08c0df080478a7ad92dd58717
solomonbaez <solomon.james.baez@gmail.com>
2023-07-20 19:16:03 -0700

    Added /version endpoint

--- api/shuttle/src/main.rs
+++ api/shuttle/src/main.rs
@@ -1,23 +1,41 @@
-use actix_web::{get, web::ServiceConfig};
+use actix_web::{get, web, web::ServiceConfig};
 use shuttle_actix_web::ShuttleActixWeb;
 use shuttle_runtime::CustomError;
-use sqlx::Executor;
+use sqlx::{Executor, PgPool};
 
 #[get("/")]
 async fn hello_world() -> &'static str {
     "Hello World!"
 }
 
+#[get("/version")]
+async fn version(db: web::Data<PgPool>) -> String {
+    let result: Result<String, sqlx::Error> = sqlx::query_scalar(
+        "
+        SELECT version()
+        ",
+    )
+    .fetch_one(db.get_ref())
+    .await;
+
+    match result {
+        Ok(version) => version,
+        Err(e) => format!("Error: {:?}", e),
+    }
+}
+
 #[shuttle_runtime::main]
 async fn actix_web(
-    #[shuttle_shared_db::Postgres()] pool: sqlx::PgPool,
+    #[shuttle_shared_db::Postgres()] pool: PgPool,
 ) -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Send + Clone + 'static> {
     pool.execute(include_str!("../../db/schema.sql"))
         .await
         .map_err(CustomError::new)?;
 
+    let pool = web::Data::new(pool);
+
     let config = move |cfg: &mut ServiceConfig| {
-        cfg.service(hello_world);
+        cfg.app_data(pool).service(hello_world).service(version);
     };
 
     Ok(config.into())
