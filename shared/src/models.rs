808ace3621a00ff0e78c145ff3ec401f5ffa7cb4
solomonbaez <solomon.james.baez@gmail.com>
2023-07-22 15:35:30 -0700

    Integrated serde w/traits

--- shared/src/models.rs
+++ shared/src/models.rs
@@ -1,6 +1,9 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Default)]
 pub struct Film {
     pub id: uuid::Uuid,
-    pub title: String, 
+    pub title: String,
     pub director: String,
     pub year: u16,
     pub poster: String,
@@ -8,6 +11,7 @@ pub struct Film {
     pub updated_at: Option<chrono::DateTime<chrono::Utc>>,
 }
 
+#[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Default)]
 pub struct CreateFilm {
     pub title: String,
     pub director: String,
