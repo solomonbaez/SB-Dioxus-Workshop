556214408123c9283bed487db059d5cc8fdadf9d
solomonbaez <solomon.james.baez@gmail.com>
2023-07-22 16:18:34 -0700

    Implemented PgFilmRepository

--- api/lib/src/film_repository/mod.rs
+++ api/lib/src/film_repository/mod.rs
@@ -1,14 +1,16 @@
 use shared::models::{CreateFilm, Film};
 use uuid::Uuid;
 
+mod pg_film_repository;
+
 pub type FilmError = String;
 pub type FilmResult<T> = Result<T, FilmError>;
 
 #[async_trait::async_trait]
 pub trait FilmRepository: Send + Sync + 'static {
     async fn get_films(&self) -> FilmResult<Vec<Film>>;
-    async fn get_film(&self, id: &Uuid) -> FilmResult<Film>;
-    async fn create_film(&self, id: &CreateFilm) -> FilmResult<Film>;
-    async fn update_film(&self, id: &Film) -> FilmResult<Film>;
-    async fn delete_film(&self, id: &Uuid) -> FilmResult<Uuid>;
+    async fn get_film(&self, film_id: &Uuid) -> FilmResult<Film>;
+    async fn create_film(&self, film_id: &CreateFilm) -> FilmResult<Film>;
+    async fn update_film(&self, film: &Film) -> FilmResult<Film>;
+    async fn delete_film(&self, film_id: &Uuid) -> FilmResult<Uuid>;
 }
--- shared/src/models.rs
+++ shared/src/models.rs
@@ -1,20 +1,26 @@
 use serde::{Deserialize, Serialize};
 
-#[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Default)]
+#[derive(
+    sqlx::FromRow, Serialize, Deserialize, Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Default,
+)]
 pub struct Film {
     pub id: uuid::Uuid,
     pub title: String,
     pub director: String,
+    #[sqlx(try_from = "i16")]
     pub year: u16,
     pub poster: String,
     pub created_at: Option<chrono::DateTime<chrono::Utc>>,
     pub updated_at: Option<chrono::DateTime<chrono::Utc>>,
 }
 
-#[derive(Serialize, Deserialize, Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Default)]
+#[derive(
+    sqlx::FromRow, Serialize, Deserialize, Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Default,
+)]
 pub struct CreateFilm {
     pub title: String,
     pub director: String,
+    #[sqlx(try_from = "i16")]
     pub year: u16,
     pub poster: String,
 }
