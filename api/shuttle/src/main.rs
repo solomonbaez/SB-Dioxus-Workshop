32e2ed24c5d3979b69df07c51458da172ef9dc72
solomonbaez <solomon.james.baez@gmail.com>
2023-07-23 17:56:59 -0700

    Integrated static file service

--- api/shuttle/src/main.rs
+++ api/shuttle/src/main.rs
@@ -2,10 +2,12 @@ use actix_web::{web, web::ServiceConfig};
 use shuttle_actix_web::ShuttleActixWeb;
 use shuttle_runtime::CustomError;
 use sqlx::{Executor, PgPool};
+use std::path::PathBuf;
 
 #[shuttle_runtime::main]
 async fn actix_web(
     #[shuttle_shared_db::Postgres()] pool: PgPool,
+    #[shuttle_static_folder::StaticFolder(folder = "static")] static_folder: PathBuf,
 ) -> ShuttleActixWeb<impl FnOnce(&mut ServiceConfig) + Send + Clone + 'static> {
     tracing::info!("Querying database");
     pool.execute(include_str!("../../db/schema.sql"))
@@ -16,9 +18,17 @@ async fn actix_web(
     let film_repository = web::Data::new(film_repository);
 
     let config = move |cfg: &mut ServiceConfig| {
-        cfg.app_data(film_repository)
-            .configure(api_lib::health_check::service)
-            .configure(api_lib::films::service::<api_lib::film_repository::PgFilmRepository>);
+        cfg.service(
+            web::scope("/api")
+                .app_data(film_repository)
+                .configure(api_lib::health_check::service)
+                .configure(api_lib::films::service::<api_lib::film_repository::PgFilmRepository>),
+        )
+        .service(
+            actix_files::Files::new("/", static_folder)
+                .show_files_listing()
+                .index_file("index.html"),
+        );
     };
 
     Ok(config.into())
