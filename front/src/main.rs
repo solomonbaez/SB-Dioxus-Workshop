0836b27dc28a13a7b6a462c62701cf30173d95ec
solomonbaez <solomon.james.baez@gmail.com>
2023-07-24 17:38:43 -0700

    Sketched out Dioxus frontend w/ wasm logging

--- front/src/main.rs
+++ front/src/main.rs
@@ -1,3 +1,15 @@
+#![allow(non_snake_case)]
+use dioxus::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    wasm_logger::init(wasm_logger::Config::default().module_prefix("front"));
+    dioxus_web::launch(App);
+}
+
+fn App(cx: Scope) -> Element {
+    cx.render(rsx! {
+        div {
+            "Hello, DevBcn!"
+        }
+    })
 }
